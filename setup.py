c4195dba334c9c0793776d2c57499317855badaf
theCyberTech - Rip&Tear <84775494+theCyberTech@users.noreply.github.com>
2024-05-21 14:34:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='crewai-logger_patch',
     version='0.1',
+    author='theCyberTech',
+    author_email='the_t3ch@pm.me',
     packages=find_packages(),
     install_requires=[
         'crewai',
